7348e3d744ae45d7bbd15f2aa1b37a9dc2a0c467
craeden <wildtron10@gmail.com>
2013-11-25 19:07:40 +0800

    created css, js and login

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var port = 80;
 var app = express();
 
 app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({secret: '7a02111d416ff52b4de9345c4185a5e9'}));
 app.use(express.compress());
 app.use(express.static(__dirname+'/..'), {maxAge : 86400000});
 
