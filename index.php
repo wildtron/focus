5d890521bf26ba1a8d13630f6538d51ae6f65ddb
ravenjohn <rjlagrimas08@gmail.com>
2013-12-27 01:44:13 +0800

    Done with navigation

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php header('location: /focus/index.html');
--- js/script.js
+++ js/script.js
@@ -1,39 +1,98 @@
 (function(root){
-    var i;
+
+    var login = function () {
+        document.getElementById('username_input').focus();
+    };
+    
+    var feed = function () {
+        var active = document.getElementsByClassName('active_section')[0];
+        if(!+active.attributes['data-order'].value){
+            active.className = '';
+            document.getElementById('feed_section').className = 'right-to-current active_section';
+        } else if (active.id !== 'feed_section'){
+            active.className = 'current-to-bottom';
+            document.getElementById('feed_section').className = 'top-to-current active_section';
+        }
+        document.getElementsByClassName('active_nav')[0].className = '';
+        document.getElementById('feed_a').className = 'active_nav';
+    };
+    var records = function (a) {
+        var active = document.getElementsByClassName('active_section')[0];
+        if(+active.attributes['data-order'].value < 2){
+            active.className = 'current-to-top';
+            document.getElementById('records_section').className = 'bottom-to-current active_section';
+        } else if(active.id !== 'records_section') {
+            active.className = 'current-to-bottom';
+            document.getElementById('records_section').className = 'top-to-current active_section';
+        }
+        document.getElementsByClassName('active_nav')[0].className = '';
+        document.getElementById('records_a').className = 'active_nav';
+    };
+    var submissions = function () {
+        var active = document.getElementsByClassName('active_section')[0];
+        if(+active.attributes['data-order'].value < 3){
+            active.className = 'current-to-top';
+            document.getElementById('submissions_section').className = 'bottom-to-current active_section';
+        } else if(active.id !== 'submissions_section') {
+            active.className = 'current-to-bottom';
+            document.getElementById('submissions_section').className = 'top-to-current active_section';
+        }
+        document.getElementsByClassName('active_nav')[0].className = '';
+        document.getElementById('submissions_a').className = 'active_nav';
+    };
+    var logs = function () {
+        var active = document.getElementsByClassName('active_section')[0];
+        if (active.id !== 'logs_section') {
+            active.className = 'current-to-top';
+            document.getElementById('logs_section').className = 'bottom-to-current active_section';
+        }
+        document.getElementsByClassName('active_nav')[0].className = '';
+        document.getElementById('logs_a').className = 'active_nav';
+    };
 
     document.getElementById('sign_in_button').onclick = function () {
         var self = this,
             username = document.getElementById('username_input'),
             password = document.getElementById('password_input');
         password.disabled = username.disabled = 'disabled';
-        if (username.value === "ravenjohn" && password.value === "ravengwapo") {
-            self.innerHTML = "SUCCESS!";
+        if (username.value === 'ravenjohn' && password.value === 'ravengwapo') {
+            self.innerHTML = 'SUCCESS!';
             $$.addClass(self, 'sign_in_success');
-            setTimeout(function () {            
-                password.disabled = username.disabled = '';
+            setTimeout(function () {
                 $$.removeClass(self, 'sign_in_success');
-                $$.addClass(document.getElementById('front_section'), 'current-to-left');
-                $$.addClass(document.getElementById('feed_section'), 'right-to-current');
-                $$.addClass(document.getElementById('nav_section'), 'left-to-current');
-                $$.addClass(document.getElementById('header_section'), 'top-to-current');
-            }, 1000);
+                password.disabled = username.disabled = '';
+                self.innerHTML = 'SIGN IN!';
+                document.getElementById('front_section').className = 'current-to-left active_section';
+                document.getElementById('nav_section').className = 'left-to-current';
+                document.getElementById('header_section').className = 'top-to-current';
+                page.show('feed');
+            }, 250);
         } else {
-            self.innerHTML = "ERROR!";
+            self.innerHTML = 'ERROR!';
             $$.addClass(self, 'sign_in_error');
             setTimeout(function () {
-                $$.removeClass(self, 'sign_in_error');
-                self.innerHTML = "SIGN IN!";
                 password.disabled = username.disabled = '';
+                $$.removeClass(self, 'sign_in_error');
+                self.innerHTML = 'SIGN IN!';
                 username.focus();
             }, 1000);
         }
     };
+    
+    var logout = function () {
+        document.getElementsByClassName('active_section')[0].className = 'current-to-right';
+        document.getElementById('front_section').className = 'left-to-current active_section';
+        document.getElementById('nav_section').className = 'current-to-left';
+        document.getElementById('header_section').className = 'current-to-top';
+        page.show('login');
+    };
 
-    /*
-    page('/records');
-    page('/records');
-    page('/submissions');
-    page('/logs');
-    page('/logout');
-    */
+    page('feed', feed);
+    page('records', records);
+    page('submissions', submissions);
+    page('logs', logs);
+    page('logout', logout);
+    page('*', login);
+
+    page.show('');
 }(this));
