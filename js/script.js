6b7568b9c6b99653c2bcba652d99a801f9ea1326
ravenjohn <rjlagrimas08@gmail.com>
2013-12-26 20:22:58 +0800

    Added header

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 (function(root){
-    var temp1 = document.getElementById('sign_in_button'),
-        i;
+    var i;
 
-    temp1.onclick = function () {
+    document.getElementById('sign_in_button').onclick = function () {
         var self = this,
-            username = document.getElementById('username_input').value,
-            password = document.getElementById('password_input').value;
-        
-        if (username === "ravenjohn" && password === "ravengwapo") {
-            
+            username = document.getElementById('username_input'),
+            password = document.getElementById('password_input');
+
+        if (username.value === "ravenjohn" && password.value === "ravengwapo") {
+            self.innerHTML = "SUCCESS!";
+            $$.addClass(self, 'sign_in_success');
+            setTimeout(function () {
+                $$.removeClass(self, 'sign_in_success');
+                $$.addClass(document.getElementById('front_section'), 'current-to-left');
+                $$.addClass(document.getElementById('feed_section'), 'right-to-current');
+                $$.addClass(document.getElementById('nav_section'), 'left-to-current');
+                $$.addClass(document.getElementById('header_section'), 'top-to-current');
+            }, 1000);
         }
         else {
             self.innerHTML = "ERROR!";
@@ -16,8 +23,14 @@
             setTimeout(function () {
                 $$.removeClass(self, 'sign_in_error');
                 self.innerHTML = "SIGN IN!";
+                username.focus();
             }, 1000);
         }
-        return false;
     };
+
+    /* page('/records');
+    page('/records');
+    page('/submissions');
+    page('/logs');
+    page('/logout'); */
 }(this));
