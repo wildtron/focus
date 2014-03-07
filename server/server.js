18c7ed479796a17b86fb16173c401dd0ca612536
ravenjohn <rjlagrimas08@gmail.com>
2014-03-07 11:30:05 +0800

    Trying to run travis

--- server/server.js
+++ server/server.js
@@ -1,34 +1,39 @@
 var express = require('express'),
     app = express(),
     fs = require('fs'),
-    logger = require(__dirname + '/lib/logger'),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
+    util = require(__dirname + '/helpers/util'),
+    logger = require(__dirname + '/lib/logger'),
+    db = require(__dirname + '/config/database'),
     router = require(__dirname + '/config/router'),
     config = require(__dirname + '/config/config').config,
-    db = require(__dirname + '/config/database'),
-    logFile = fs.createWriteStream(__dirname + '/logs/' + new Date().toJSON().substring(0, 10) + '.log', {flags: 'a'});
+    logFile;
 
-logger.log('info', 'initializing FOCUS...');
-app.use(express.logger({stream : logFile}));
-app.use(express.compress());
-app.use(express.limit('25mb'));
-app.use(express.bodyParser({uploadDir : __dirname + '/temp'}));
-app.use(express.methodOverride());
-app.use(express.cookieParser(config.COOKIE_SECRET));
-app.use(express.static(__dirname + '/public'));
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+util.mkdir(__dirname + '/logs', function () {
+	logFile = fs.createWriteStream(__dirname + '/logs/' + new Date().toJSON().substring(0, 10) + '.log', {flags: 'a'});
+	logger.log('info', 'initializing FOCUS...');
+	app.use(express.logger({stream : logFile}));
+	app.use(express.compress());
+	app.use(express.limit('25mb'));
+	app.use(express.bodyParser({uploadDir : __dirname + '/temp'}));
+	app.use(express.methodOverride());
+	app.use(express.cookieParser(config.COOKIE_SECRET));
+	app.use(express.static(__dirname + '/public'));
+	app.use(function (req, res, next) {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+		res.setHeader('Access-Control-Allow-Credentials', true);
+		next();
+	});
+	app.use(app.router);
+
+	logger.log('verbose', 'setting up router');
+	router.setup(app);
+	logger.log('verbose', 'handling sockets');
+	router.handleSocket(io);
+
+	db.listenOnConnect(server);
 });
-app.use(app.router);
 
-logger.log('verbose', 'setting up router');
-router.setup(app);
-logger.log('verbose', 'handling sockets');
-router.handleSocket(io);
-
-db.listenOnConnect(server);
