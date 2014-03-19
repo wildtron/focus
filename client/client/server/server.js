dbdd0f44a698b1da957fac4f6908411fd7423d62
wildtron <wildtron10@gmail.com>
2014-03-19 10:24:10 +0800

    fixed proces to process

--- client/client/server/server.js
+++ client/client/server/server.js
@@ -131,7 +131,7 @@ fs.chmodSync(__dirname+'/scripts/enable.sh',0555);
 fs.chmodSync(__dirname+'/scripts/linux-app-arm',0555);
 fs.chmodSync(__dirname+'/scripts/mouse.sh',0555);
 fs.chmodSync(__dirname+'/scripts/shot.py',0555);
-if(proces.arch.slice(-2) === '64'){
+if(process.arch.slice(-2) === '64'){
     fs.chmodSync(__dirname+'/apparmor/bin/apparmor64', 0555);
 }
 else {
