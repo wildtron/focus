e0f767cb2c482d9ed45778034402b1fc87e3f28b
ravenjohn <rjlagrimas08@gmail.com>
2014-03-24 11:37:32 +0800

    Fixed server url

--- server/public/js/script.js
+++ server/public/js/script.js
@@ -11,7 +11,7 @@
 		chatBlinkIntervals = {},
         url = 'http://10.0.5.49:8080/',
         // url = 'http://192.168.1.55:8080/',
-        url = 'http://ricolindo.uplb.edu.ph:8080/',
+        // url = 'http://ricolindo.uplb.edu.ph:8080/',
 
 		/**
 			Page Actions
