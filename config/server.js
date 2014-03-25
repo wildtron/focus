d46180078bcdb431c3b566b2e8e4be5c34986117
ravenjohn <rjlagrimas08@gmail.com>
2014-03-25 14:59:31 +0800

    Fixed config server to allow CORS

--- config/server.js
+++ config/server.js
@@ -1,11 +1,12 @@
-var connect = require('connect');
-
-connect.createServer(
-	function (req, res, next) {
+var http = require('http'),
+	connect = require('connect'),
+	app = connect()
+    .use(connect.static(__dirname + '/public'))
+    .use(function(req, res){
 		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
 		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
-		next();
-	},
-	connect.static(__dirname + '/public')
-).listen(8081);
+		res.end('hello world\n');
+	});
+
+http.createServer(app).listen(8081);
