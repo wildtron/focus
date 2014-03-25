8b34dd331efde7020624b36a032789bf4f803adb
ravenjohn <rjlagrimas08@gmail.com>
2014-03-25 15:05:01 +0800

    Fixed config server CORS

--- config/server.js
+++ config/server.js
@@ -1,12 +1,12 @@
 var http = require('http'),
 	connect = require('connect'),
 	app = connect()
-    .use(connect.static(__dirname + '/public'))
-    .use(function(req, res){
+    .use(function (req, res, next) {
 		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
 		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
-		res.end('hello world\n');
-	});
+		next();
+	})
+    .use(connect.static(__dirname + '/public'));
 
 http.createServer(app).listen(8081);
