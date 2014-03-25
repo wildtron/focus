ccf544397b06482daa1016be42569bb82b924636
ravenjohn <rjlagrimas08@gmail.com>
2014-03-25 14:35:39 +0800

    Added access control allows on config server

--- config/server.js
+++ config/server.js
@@ -1,5 +1,11 @@
-var http = require('http'),
-	connect = require('connect'),
-	app = connect().use(connect.static(__dirname + '/public'));
+var connect = require('connect');
 
-http.createServer(app).listen(8081);
+connect.createServer(
+	function (req, res, next) {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+		next();
+	},
+	connect.static(__dirname + '/public')
+).listen(8081);
