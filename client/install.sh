194b783f20a4b3107dae85f5ef6e1d801359a460
wildtron <wildtron10@gmail.com>
2014-04-16 11:53:02 +0800

    updated gitignore

--- client/install.sh
+++ client/install.sh
@@ -6,11 +6,7 @@ if [[ $EUID -eq 0 ]]; then
     mkdir /opt/focus
     loc=`dirname $BASH_SOURCE`
     /bin/cp -rvf $loc/* /opt/focus
-    if [[ -d "/etc/profile.d/focus.sh" ]]; then
-        rm /etc/profile.d/focus.sh
-    fi
-    echo "#/usr/bin/env bash" > /etc/profile.d/focus.sh
-    echo "sudo /opt/focus/run" >> /etc/profile.d/focus.sh
+
     exit
 else
     echo "This script should be run as root"
